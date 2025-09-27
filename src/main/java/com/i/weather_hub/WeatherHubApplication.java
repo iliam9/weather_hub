71c120e8308e1c987fc69b6b15b3250550674118
iliam9 <ilkafa93@yandex.ru>
2025-09-27 23:55:37 +0300

    Initial commit: Generate project from Spring Initializr

---
+++ src/main/java/com/i/weather_hub/WeatherHubApplication.java
@@ -0,0 +1,13 @@
+package com.i.weather_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherHubApplication.class, args);
+	}
+
+}
